018b36d3b261dd2783face20d0961795540e8e57
novemberrain123 <pleaseworkusob@gmail.com>
2022-12-11 16:03:50 +0800

    remove secret key

--- pingproj/settings.py
+++ pingproj/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@7zds&jvn)q6_jvdly^9+h$p=&9@6%!k!v0gg6rua_&ctu*kr3"
+from decouple import python-decouple
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
